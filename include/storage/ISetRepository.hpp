a6d986d0e3663b946c0f41e2dfbea5476afd9990
dan-kst <stupakdanielo@gmail.com>
2025-12-25 15:03:21 +0200

    fix(storage): Add #ifndef directive
    Helps prevent multiple inclusions of the same header file

--- include/storage/ISetRepository.hpp
+++ include/storage/ISetRepository.hpp
@@ -1,3 +1,5 @@
+#ifndef ISETREPOSITORY_HPP
+#define ISETREPOSITORY_HPP
 #include "core/IntegerSet.hpp"
 
 class ISetRepository {
@@ -6,3 +8,4 @@ public:
     virtual void save(const IntegerSet& set, int id) = 0;
     virtual std::unique_ptr<IntegerSet> load(int id) = 0;
 };
+#endif
