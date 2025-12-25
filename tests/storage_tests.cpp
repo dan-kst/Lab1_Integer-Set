be04a9e4c0f5fc0d693d766c62dcc6aed6f7ff0e
dan-kst <stupakdanielo@gmail.com>
2025-12-25 18:50:26 +0200

    refacctor(tests): Update mock class inherited members.

--- tests/storage_tests.cpp
+++ tests/storage_tests.cpp
@@ -6,8 +6,8 @@
 // Create the Mock class
 class MockSetRepository : public ISetRepository {
 public:
-    MOCK_METHOD(void, save, (const IntegerSet& set, int id), (override));
-    MOCK_METHOD(std::unique_ptr<IntegerSet>, load, (int id), (override));
+    MOCK_METHOD(size_t, save, (const IntegerSet& set), (override));
+    MOCK_METHOD(std::unique_ptr<IntegerSet>, load, (size_t id), (override));
 };
 
 TEST(StorageTest, AppCallsSaveMethod) {
@@ -15,17 +15,17 @@ TEST(StorageTest, AppCallsSaveMethod) {
     MockSetRepository mockRepo;
 
     // We EXPECT the save method to be called exactly 1 time with ID 99
-    EXPECT_CALL(mockRepo, save(testing::_, 99)).Times(1);
+    EXPECT_CALL(mockRepo, save(testing::_)).Times(1);
 
     // Act
-    mockRepo.save(mySet, 99); 
+    mockRepo.save(mySet); 
 }
 TEST(StorageTest, PostgresRealSaveLoad) {
     // This test requires Docker to be running!
     PostgresRepository repo;
     IntegerSet original = {42, 100, 7};
     
-    repo.save(original, 1);
+    repo.save(original);
     auto loaded = repo.load(1);
     
     EXPECT_EQ(loaded->size(), 3);
