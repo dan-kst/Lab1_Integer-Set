fde779a4bca00a5e9b0e52974a6b9272347522d2
dan-kst <stupakdanielo@gmail.com>
2025-12-27 12:13:46 +0200

    refactor(storage): Store JSON value name as a separate variable
    Improve code maintaining and readability by removing "magic" strings.

--- include/storage/IntegerSetSerializer.hpp
+++ include/storage/IntegerSetSerializer.hpp
@@ -3,8 +3,11 @@
 
 #include "core/IntegerSet.hpp"
 #include <nlohmann/json.hpp>
+#include <string>
+#include <memory>
 
 namespace SetSerializer {
+    static std::string valueName = "elements"; 
     nlohmann::json to_json(const IntegerSet& set);
     std::unique_ptr<IntegerSet> from_json(const nlohmann::json& j);
 }
--- src/storage/IntegerSetSerializer.cpp
+++ src/storage/IntegerSetSerializer.cpp
@@ -3,12 +3,12 @@
 namespace SetSerializer{
     nlohmann::json to_json(const IntegerSet& set)
     {
-        return nlohmann::json{{"elements", set.toVector()}};
+        return nlohmann::json{{valueName, set.toVector()}};
     }
     std::unique_ptr<IntegerSet> from_json(const nlohmann::json& j)
     {
         std::unique_ptr<IntegerSet> set = std::make_unique<IntegerSet>();
-        auto vec = j.at("elements").get<std::vector<int>>();
+        auto vec = j.at(valueName).get<std::vector<int>>();
         for(int x : vec) 
         {
             set->add(x);
