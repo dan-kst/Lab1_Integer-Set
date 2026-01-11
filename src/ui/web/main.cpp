7d339af3ed1e22ac2d9b1265280d474c52d410c1
dan-kst <stupakdanielo@gmail.com>
2026-01-11 14:36:02 +0200

    feature(ui/web): Implement the basic "/sets" route to verify connection between the server and database.

--- src/ui/web/main.cpp
+++ src/ui/web/main.cpp
@@ -1,11 +1,26 @@
 #include "crow.h"
+#include "storage/PostgresRepository.hpp"
+#include "ui/common/WrapCore.hpp"
+#include <memory>
 
 int main()
 {
+    auto repo = std::make_shared<PostgresRepository>();
+    auto currentSet = std::make_shared<IntegerSet>();
+    WrapCore core(repo, currentSet);
     crow::SimpleApp app;
 
     CROW_ROUTE(app, "/")(
         [](){ return "Integer Set Lab API is Online!";}
+    );
+    
+    CROW_ROUTE(app, "/sets")(
+        [&core]()
+        {
+            auto ids = core.getIdList();
+            nlohmann::json j = ids;
+            return crow::response(j.dump());
+        }
     );
 
     app.port(18080).multithreaded().run();
